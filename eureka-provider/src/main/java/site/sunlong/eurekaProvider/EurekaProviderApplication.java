dc81e89789f1c3b5b2cb5ead050cea44c70fc15b
sunlong <17717288326@163.com>
2021-11-17 19:13:33 +0800

    eureka-provider

---
+++ eureka-provider/src/main/java/site/sunlong/eurekaProvider/EurekaProviderApplication.java
@@ -0,0 +1,13 @@
+package site.sunlong.eurekaProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProviderApplication.class, args);
+    }
+
+}
