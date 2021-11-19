478c2169ed933cb9a068827e9cae0931b40f3762
sunlong <17717288326@163.com>
2021-11-19 19:15:51 +0800

    hystrix dashboard

---
+++ hystrix-dashboard/src/main/java/site/sunlong/hystrixDashboard/HystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package site.sunlong.hystrixDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
