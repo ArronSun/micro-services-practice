259b959e59c245b17b62c62ef919c58d52d6864b
sunlong <17717288326@163.com>
2021-11-26 10:06:16 +0800

    hystrix dashboard

--- eureka-provider/src/main/java/site/sunlong/eurekaProvider/EurekaProviderApplication.java
+++ eureka-provider/src/main/java/site/sunlong/eurekaProvider/EurekaProviderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 //hystrix
 @EnableDiscoveryClient
