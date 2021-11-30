cb987ffe3c0e81efda19079299aa65826b446cad
sunlong <17717288326@163.com>
2021-11-30 18:53:52 +0800

    zuul

--- eureka-consumer/src/main/java/site/sunlong/eurekaConsumer/service/ProviderService.java
+++ eureka-consumer/src/main/java/site/sunlong/eurekaConsumer/service/ProviderService.java
@@ -3,15 +3,15 @@ package site.sunlong.eurekaConsumer.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import site.sunlong.eurekaConsumer.service.fallback.ProviderFallbackFactory;
 import site.sunlong.eurekaConsumer.service.fallback.ProviderServiceFallback;
 
 /**
+ * fallback = ProviderServiceFallback.class 不能获取具体异常
  * @Author: Sunlong
  * @date: 2021/11/18 18:12
  */
-@FeignClient(name = "PROVIDER" , path = "provider" , fallback = ProviderServiceFallback.class)
+//@FeignClient(name = "PROVIDER" , path = "provider" , fallback = ProviderServiceFallback.class)
+@FeignClient(name = "ZUUL-GATEWAY" , path = "zuul-proxy/providers-proxy/provider" , fallback = ProviderServiceFallback.class)
 //@FeignClient(name = "PROVIDER" , path = "provider" , fallbackFactory = ProviderFallbackFactory.class)
 public interface ProviderService {
 
